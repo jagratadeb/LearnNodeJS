d887c236b5658f5773cea82aea30bd6a4b97f41e
Jagrata Deb <jagratadeb2004@gmail.com>
2026-03-31 16:42:27 +0530

    add code for parsing query parameter

--- Module 5 (Web Server)/index.js
+++ Module 5 (Web Server)/index.js
@@ -1,15 +1,21 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()}: ${req.url} New Request received\n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("./Module 5 (Web Server)/log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("home page");
         break;
       case "/about":
-        res.end("I am an SDE\nJagrata Deb");
+        const name = myUrl.query.name;
+        const uid = myUrl.query.uid;
+        res.end(`Hi ${name}\n your uid is ${uid}`);
         break;
       default:
         res.end("404 Not Found!");
