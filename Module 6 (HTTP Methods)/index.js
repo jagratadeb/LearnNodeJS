213784f49733d8319aa7c258ca09d80ba2368e22
Jagrata Deb <jagratadeb2004@gmail.com>
2026-04-25 11:08:54 +0530

    add code got HTTP Methods

---
+++ Module 6 (HTTP Methods)/index.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()}: ${req.url} ${req.method} New Request received\n`;
+  const myUrl = url.parse(req.url, true);
+  const logFilePath = path.join(__dirname, "log.txt");
+  console.log(myUrl);
+  fs.appendFile(logFilePath, log, (err) => {
+    if (err) {
+      console.error("Failed to write log:", err.message);
+    }
+
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("home page");
+        break;
+      case "/about":
+        const name = myUrl.query.name;
+        const uid = myUrl.query.uid;
+        res.end(`Hi ${name}\n your uid is ${uid}`);
+        break;
+      default:
+        res.end("404 Not Found!");
+    }
+  });
+});
+
+server.listen(4000, () => {
+  console.log("Server started!");
+});
